c48365166bd8f25afa2cdc544146ca11c9e46c74
medi_rudiant <medirudiantoni2@gmail.com>
2023-08-26 14:43:53 +0700

    hmm

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function movieCard(movie) {
 }
 
 function movieDetailModal(movie) {
-  return `<div class="container containerDetail overflow-y-scroll h-screen md:h-[600px] bg-slate-800 relative p-5 md:p-10">
+  return `<div class="container containerDetail h-screen md:h-[600px] bg-slate-800 relative p-5 md:p-10">
                 <div class="modalCloser absolute top-0 right-0 py-2 px-4 bg-red-600 hover:bg-red-700 active:bg-red-800 cursor-pointer">Close</div>
 
-                <div class=" w-full h-full flex flex-col md:flex-row gap-5 justify-start md:pt-10">
+                <div class=" w-full h-full flex flex-col md:flex-row gap-5 justify-start md:pt-10 overflow-y-scroll sm:overflow-hidden">
                     <div class="w-[300px] h-full flex">
                         <img src="${movie.Poster}" alt="poster" class="w-full h-fit object-cover">
                     </div>
@@ -81,7 +81,8 @@ document.addEventListener("click", function (e) {
     document.body.style.overflowY = 'hidden';
     const imdbid = e.target.getAttribute("data-imdbid");
     overlayModal.innerHTML = loadingSpinAnimation();
-    overlayModal.classList.add("translate-y-full");
+    overlayModal.classList.add("translate-y-[195%]");
+    overlayModal.classList.add("sm:translate-y-full");
     const movieDetail =
       fetch(`https://www.omdbapi.com/?apikey=54ef65bc&i=${imdbid}
         `);
@@ -97,7 +98,8 @@ document.addEventListener("click", function (e) {
 document.addEventListener("click", function (e) {
   if (e.target.classList.contains("modalCloser")) {
     document.body.style.overflowY = 'scroll';
-    overlayModal.classList.remove("translate-y-full");
+    overlayModal.classList.remove("translate-y-[195%]");
+    overlayModal.classList.remove("sm:translate-y-full");
     overlayModal.innerHTML = "";
   }
 });
